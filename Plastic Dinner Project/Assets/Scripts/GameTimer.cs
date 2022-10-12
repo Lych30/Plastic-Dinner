3941c3959f1942671c56871245ef2132561e18da
Paul Putegnat <paul.putegnat@edu.devinci.fr>
2022-10-12 11:59:06 +0200

    Finished Timers and added UI Prefabs

--- Plastic Dinner Project/Assets/Scripts/GameTimer.cs
+++ Plastic Dinner Project/Assets/Scripts/GameTimer.cs
@@ -5,24 +5,79 @@ using TMPro;
 
 public class GameTimer : MonoBehaviour
 {
-    private float currentTime = 0f;
-    private float startingTime = 10f;
-    private TMP_Text timerText;
+    [SerializeField]
+    public float timerDuration;
+    
+    [SerializeField]
+    private bool countDown = true;
 
-    void Start()
+    public float timer;
+    [SerializeField]
+    private TextMeshProUGUI firstMinute;
+    [SerializeField]
+    private TextMeshProUGUI secondMinute;
+    [SerializeField]
+    private TextMeshProUGUI separator;
+    [SerializeField]
+    private TextMeshProUGUI firstSecond;
+    [SerializeField]
+    private TextMeshProUGUI secondSecond;
+
+    private void Awake()
     {
-        timerText = GetComponentInChildren<TMP_Text>();
-        currentTime = startingTime;
+        ResetTimer();
+    }
+
+    private void ResetTimer()
+    {
+        if (countDown)
+        {
+            timer = timerDuration;
+        }
+        else
+        {
+            timer = 0;
+        }
+        SetTextDisplay(true);
     }
 
     void Update()
     {
-        currentTime -= 1 * Time.deltaTime;
-        timerText.text = currentTime.ToString("0");
-
-        if(currentTime <= 0)
+        if (countDown && timer > 0)
         {
-            currentTime = 0;
+            timer -= Time.deltaTime;
+            UpdateTimerDisplay(timer);
         }
+        else if (!countDown && timer < timerDuration)
+        {
+            timer += Time.deltaTime;
+            UpdateTimerDisplay(timer);
+        }
+    }
+
+    private void UpdateTimerDisplay(float time)
+    {
+        if (time < 0)
+        {
+            time = 0;
+        }
+
+        float minutes = Mathf.FloorToInt(time / 60);
+        float seconds = Mathf.FloorToInt(time % 60);
+
+        string currentTime = string.Format("{00:00}{01:00}", minutes, seconds);
+        firstMinute.text = currentTime[0].ToString();
+        secondMinute.text = currentTime[1].ToString();
+        firstSecond.text = currentTime[2].ToString();
+        secondSecond.text = currentTime[3].ToString();
+    }
+
+    private void SetTextDisplay(bool enabled)
+    {
+        firstMinute.enabled = enabled;
+        secondMinute.enabled = enabled;
+        separator.enabled = enabled;
+        firstSecond.enabled = enabled;
+        secondSecond.enabled = enabled;
     }
 }
--- Plastic Dinner Project/Assets/Scripts/OrderTimer.cs
+++ Plastic Dinner Project/Assets/Scripts/OrderTimer.cs
@@ -6,14 +6,38 @@ using UnityEngine.UI;
 public class OrderTimer : MonoBehaviour
 {
     [SerializeField] private Image uiFiller;
+    private GameTimer gameTimer;
 
-    public int duration;
-
+    private int orderDuration;
     private int remaningDuration;
+    private float tColor;
 
     void Start()
     {
-        Begin(duration);
+        gameTimer = transform.parent.GetComponent<GameTimer>();
+        if (gameTimer.timer > gameTimer.timerDuration * (3 / 4))
+        {
+            orderDuration = 10;
+        }
+        else if (gameTimer.timer > gameTimer.timerDuration * (2 / 4))
+        {
+            orderDuration = 8;
+        }
+        else if (gameTimer.timer > gameTimer.timerDuration * (1 / 4))
+        {
+            orderDuration = 6;
+        }
+        else
+        {
+            orderDuration = 4;
+        }
+        Begin(orderDuration);
+    }
+
+    private void Update()
+    {
+        tColor += Time.deltaTime / remaningDuration;
+        uiFiller.color = Color.LerpUnclamped(Color.green, Color.red, tColor);
     }
 
     private void Begin (int second)
@@ -26,7 +50,7 @@ public class OrderTimer : MonoBehaviour
     {
         while (remaningDuration >= 0)
         {
-            uiFiller.fillAmount = Mathf.InverseLerp(0, duration, remaningDuration);
+            uiFiller.fillAmount = Mathf.InverseLerp(0, orderDuration, remaningDuration);
             remaningDuration--;
             yield return new WaitForSeconds(1);
         }
