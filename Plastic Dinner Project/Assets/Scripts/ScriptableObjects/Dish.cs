26d5210a03602f4ff394ff0ad838bedbfab9646d
nguyen <virginie.nguyen@edu.devinci.fr>
2022-10-25 13:28:34 +0200

    delete Dish.cs (not use)

--- Plastic Dinner Project/Assets/Scripts/ScriptableObjects/Dish.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "New Dish", menuName = "ScriptableObjects/Dish")]
-public class Dish : ScriptableObject
-{
-    public List<Food> foods = new List<Food>();
-}
